b4bb5471787b6906f15ec470449d7bd47de46cd8
nantonel <nantonel@esat.kuleuven.be>
2016-12-08 16:47:48 +0100

    test computation reduced

--- test/test_ism.jl
+++ test/test_ism.jl
@@ -5,7 +5,7 @@ using Base.Test
 
 Fs = 4e4                    # Sampling frequency
 env = AcEnv(Fs)             # create new acoustic env with default values
-Nt = round(Int64,0.6*Fs)    # Number of time samples, 0.25 secs
+Nt = round(Int64,0.1*Fs)    # Number of time samples, 0.25 secs
 Lx,Ly,Lz  = 4.,4.,4.# Room dimensions
 xs = [2 1.5 1]'      # Source position
 s = randn(Nt,size(xs,2))
@@ -13,7 +13,7 @@ s = zeros(s)
 s[1,:] = 1
 xr = [1. 2. 2. ]'   # Receiver position
 
-T60 = .5           # Reverberation Time
+T60 = .1           # Reverberation Time
 geo = CuboidRoom(Lx,Ly,Lz,T60,env)
 #test printing
 show(geo)
@@ -22,19 +22,20 @@ Tw = 20              # samples of Low pass filter
 Fc = 0.9             # cut-off frequency
 
 println("testing β for a given T60 gives correct T60")
-@time h  = rim(xs,xr,Nt,geo,env)
+@time h  = rim(xs,xr,Nt,geo,env; N = [4;4;4])
 rt,edc = revTime(h,env)
 @test norm(rt-T60)/norm(T60)< 0.25 #testing rev time
 
 println("testing seed for randomization is preserved and multi source")
-xr = LinearGrid([Lx/2;Ly/2;Lz/2],[0.5],3) #3 mic linear array positioned at the center of the room
-xs = SphFib([Lx/2;Ly/2;Lz/2],0.9*Lz/2,5)
+xr = LinearGrid([Lx/2;Ly/2;Lz/2],[0.5],2) #2 mic linear array positioned at the center of the room
+xs = SphFib([Lx/2;Ly/2;Lz/2],0.9*Lz/2,2)
+Nt = round(Int64,0.03*Fs)    # Number of time samples, 0.25 secs
 s = randn(Nt,xs.Nm)
-@time h = rim(s,xs,xr,Nt,geo,env;N=[3;3;3])
+@time h = rim(s,xs,xr,Nt,geo,env;N=[2;2;2])
 ## generate another IR with same randomization and
 h2 = zeros(Nt,xr.Nm)
 for i = 1:xs.Nm 
-	hh = rim(xs.pos[:,i],xr,Nt,geo,env;N=[3;3;3])
+	hh = rim(xs.pos[:,i],xr,Nt,geo,env;N=[2;2;2])
 	for ii = 1:xr.Nm
 		h2[:,ii] += conv(s[:,i],hh[:,ii])[1:Nt]
 	end
@@ -45,7 +46,7 @@ end
 println("test with no frac delay")
 xs = [1.;1.;1.]
 xr = [2 1.5 1]'      # Source position
-@time h2 = rim(xs,xr,Nt,geo,env; Tw = 0, Fc = 0., N = [4;4;4])
+@time h2 = rim(xs,xr,Nt,geo,env; Tw = 0, Fc = 0., N = [2;2;2])
 
 #using PyPlot
 #t = linspace(0,Nt*1/Fs,Nt)
